5120a3d0b525e4196c59b1c860eeda535decd63e
enodal <164991691+enodal@users.noreply.github.com>
2024-04-09 19:21:22 +0000

    done

--- Diceroll.rb
+++ Diceroll.rb
@@ -41,9 +41,9 @@ get("/dice/2/10") do
 end
 
 get("/dice/1/20") do
-  die = rand(1..20)
+  @die = rand(1..20)
 	
-	@outcome = "You rolled a #{die}."
+	@outcome = "You rolled a #{@die}."
 	
 	erb(:one_twenty,{:layout => :wrapper})
 end
@@ -58,4 +58,16 @@ get("/dice/5/4") do
 	@outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
 	
   erb(:five_four,{:layout => :wrapper})
+end
+
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
 end
